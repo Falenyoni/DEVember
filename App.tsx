af08373fd1f6a7a303944b28509f03befcf5ee23
Bongani Nyoni <b.nyoni1101@gmail.com>
2023-12-18 13:09:40 +0200

    Adding FlatList to Advent Calender

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
+
+const days = [...Array(24)].map((val, index) => index + 1)
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <Text>#DEVember</Text>
+      <FlatList 
+        data={days}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={4}
+        renderItem={({item}) =>
+          <DayListItem  day={item}/>
+        }/>
+
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -16,7 +27,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    
+  },
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+  column: {
+    gap: 10,
   },
 });
